e4a66215599f719a87686c9ffd05cd9bec86b4b9
Kirill Serebryannyy <sincerely.manny@gmail.com>
2024-12-25 00:13:39 +0300

    fix: fixed kv store type definition

--- src/hooks/use-kv-store.ts
+++ src/hooks/use-kv-store.ts
@@ -36,5 +36,5 @@ export function useKvStore<K extends TKey>(key: K) {
     set(memoValue);
   }, [memoValue, set]);
 
-  return [memoValue, setMemoValue] as const;
+  return [memoValue, setMemoValue] as [TData<K>, typeof setMemoValue];
 }
