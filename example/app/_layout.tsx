021590ae950478cc9290e0c8ab79a17a920204cf
Kirill Serebryannyy <sincerely.manny@gmail.com>
2025-01-06 17:20:05 +0300

    feat: implemented fade out view

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,9 +1,6 @@
 import { Stack } from 'expo-router';
 import { config } from 'expo-with-pincode';
 
-import { useSelector } from '@xstate/store/react';
-import { store } from 'expo-with-pincode/store';
-import { useEffect } from 'react';
 import { PincodeAuthScreen } from '../components/pincode-auth-screen';
 import { PincodeSetScreen } from '../components/pincode-set-screen';
 
@@ -14,10 +11,5 @@ config({
 });
 
 export default function RootLayout() {
-  const mutex = useSelector(store, (state) => state.context.session.mutex);
-
-  useEffect(() => {
-    console.log('mutex', mutex);
-  }, [mutex]);
   return <Stack />;
 }
